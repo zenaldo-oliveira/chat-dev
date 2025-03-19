21a8e828875eeedf4a00db3c5a02f4af45bb02d8
Zenaldo Oliveira <zenaldo.221@hotmail.com>
2025-03-18 20:59:32 -0400

    first  commit

---
+++ backend/src/server.js
@@ -0,0 +1,10 @@
+const { WebSocketServer } = require("ws");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const wss = new WebSocketServer({ port: process.env.PORT || 8080 });
+
+wss.on("", (connection) => {
+  wss.on("error", console.error);
+});
