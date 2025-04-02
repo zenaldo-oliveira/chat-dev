30bf390e0eb7d35c8c2cd3dd2add3ac61c14e4aa
Zenaldo Oliveira <zenaldo.221@hotmail.com>
2025-04-01 20:25:46 -0400

    Primeiro commit

---
+++ src/server.js
@@ -0,0 +1,18 @@
+const { WebSocketServer } = require("ws");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const wss = new WebSocketServer({ port: process.env.PORT || 8080 });
+
+wss.on("connection", (ws) => {
+  ws.on("error", console.error);
+
+  ws.on("message", (data) => {
+    wss.clients.forEach((client) => client.send(data.toString()));
+  });
+
+  console.log("Client connected");
+});
+
+console.log("WebSocket server running on port", process.env.PORT || 8080);
