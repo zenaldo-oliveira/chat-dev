5f94ae33d7cc46696a5fadeb0b3630f22ef8d756
Zenaldo Oliveira <zenaldo.221@hotmail.com>
2025-04-05 23:09:14 -0400

    alterando server url

--- frontend/script.js
+++ frontend/script.js
@@ -90,7 +90,7 @@ const handleSubmit = (event) => {
   login.style.display = "none";
   chat.style.display = "flex";
 
-  websocket = new WebSocket("ws://localhost:8080");
+  websocket = new WebSocket("wss://chat-backend-63ie.onrender.com");
   websocket.onmessage = processMessage;
 };
 const sendMessage = (event) => {
