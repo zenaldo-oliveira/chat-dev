4f0dd43095bcc76aab389f7811145ba90811ee6d
Zenaldo Oliveira <zenaldo.221@hotmail.com>
2025-04-06 18:29:10 -0400

    alteração url ws

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,37 +1,16 @@
-const express = require("express");
-const http = require("http");
 const { WebSocketServer } = require("ws");
 const dotenv = require("dotenv");
-const path = require("path");
 
 dotenv.config();
 
-const PORT = process.env.PORT || 8080;
-
-const app = express();
-const server = http.createServer(app);
-const wss = new WebSocketServer({ server });
-
-app.get("/", (req, res) => {
-  res.send("Servidor WebSocket está rodando!");
-});
+const wss = new WebSocketServer({ port: process.env.PORT || 8080 });
 
 wss.on("connection", (ws) => {
-  console.log("✅ Novo cliente conectado");
+  ws.on("error", console.error);
 
   ws.on("message", (data) => {
-    console.log("📨 Mensagem recebida:", data.toString());
-
-    wss.clients.forEach((client) => {
-      if (client.readyState === ws.OPEN) {
-        client.send(data.toString());
-      }
-    });
+    wss.clients.forEach((client) => client.send(data.toString()));
   });
 
-  ws.on("error", (err) => console.error("❌ Erro WebSocket:", err));
-});
-
-server.listen(PORT, () => {
-  console.log(`🚀 Servidor rodando em http://localhost:${PORT}`);
+  console.log("client connected");
 });
