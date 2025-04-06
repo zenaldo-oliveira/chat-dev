d99f3cbfdafe7878779486f163dfdef666e90296
Zenaldo Oliveira <zenaldo.221@hotmail.com>
2025-04-06 09:46:37 -0400

    atualização url ws

--- frontend/script.js
+++ frontend/script.js
@@ -83,7 +83,8 @@ const handleSubmit = (event) => {
   login.style.display = "none";
   chat.style.display = "flex";
 
-  websocket = new WebSocket("ws://localhost:8080");
+  websocket = new WebSocket("wss://chat-backend-63ie.onrender.com");
+
   websocket.onmessage = processMessage;
 };
 
